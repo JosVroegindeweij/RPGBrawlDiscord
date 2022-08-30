75f0e3bf81f8316e6317f264a6b5bb53c9cfa465
Unknown <jos.vroegindeweij@hotmail.com>
2022-08-30 09:17:52 +0200

    Intent fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const Logger = require('./utils/logger');
 const cmdHandler = require('./utils/commandHandler');
 
 const {token} = require('./secrets/config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.MESSAGE_CONTENT]});
+const client = new Client({ intents: [GatewayIntentBits.GUILDS, GatewayIntentBits.GUILD_MESSAGES,
+        GatewayIntentBits.GUILD_MESSAGE_REACTIONS, GatewayIntentBits.MESSAGE_CONTENT]});
 
 client.once('ready', () => {
     info('Bot launched!', 'main');
